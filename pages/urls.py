3a2eccbd2feb9235add74eb427dd99c4e4bbd615
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-06-14 12:40:32 +0200

    a lot of edits

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -7,7 +8,19 @@ urlpatterns = [
  path('', views.home, name='home'),
 path('location/', views.location, name='location'),
 path('landing/', views.landing, name='landing'),
-
+path('food-home/', views.food_home, name='food-home'),
+path('grocery-home/', views.grocery_home, name='grocery-home'),
+path('pharmacy-home/', views.pharmacy_home, name='pharmacy-home'),
+path('profile', views.profile, name='profile'),
+path('profile-settings', views.profile_settings, name='profile-settings'),
+path('manage-delivery-address', views.manage_delivery_address, name='manage-delivery-address'),
+path('wishlist', views.wishlist, name='wishlist'),
+path('manage-payment', views.manage_payment, name='manage-payment'),
+path('order-history', views.order_history, name='order-history'),
+path('voucher', views.voucher, name='voucher'),
+path('app-setting', views.app_setting, name='app-setting'),
+path('notification-setting', views.notification_setting, name='notification-setting'),
+path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -12,3 +12,56 @@ def location(request):
 @login_required
 def landing(request):
     return render(request, 'pages/landing.html')
+
+@login_required
+def food_home(request):
+    return render(request, 'pages/food-home.html')
+
+@login_required
+def grocery_home(request):
+    return render(request, 'pages/grocery-home.html')
+
+@login_required
+def pharmacy_home(request):
+    return render(request, 'pages/pharmacy-home.html')
+
+@login_required
+def profile(request):
+    return render(request, 'pages/profile.html')
+
+@login_required
+def profile_settings(request):
+    return render(request, 'pages/profile-settings.html')
+
+@login_required
+def manage_delivery_address(request):
+    return render(request, 'pages/manage-delivery-address.html')
+
+@login_required
+def wishlist(request):
+    return render(request, 'pages/wishlist.html')
+
+@login_required
+def manage_payment(request):
+    return render(request, 'pages/manage-payment.html')
+
+@login_required
+def order_history(request):
+    return render(request, 'pages/order-history.html')
+
+@login_required
+def voucher(request):
+    return render(request, 'pages/voucher.html')
+
+@login_required
+def app_setting(request):
+    return render(request, 'pages/app-setting.html')
+
+@login_required
+def notification_setting(request):
+    return render(request, 'pages/notification-setting.html')
+
+@login_required
+def logout(request):
+    return render(request, 'pages/logout.html')
+
