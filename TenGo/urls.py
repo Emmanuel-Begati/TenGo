09a8e33d7c6b025b90f945df62cbc45b37f9a8d6
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-06-15 18:47:47 +0200

    addded and removed some files

--- TenGo/urls.py
+++ TenGo/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import customer.views as views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.base, name='home'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'customer/index.html')
+
+def about(request):
+    return render(request, 'customer/about.html')
+
+def contact(request):
+    return render(request, 'customer/contact.html')
+
+def base(request):
+    return render(request, 'customer/base.html')
