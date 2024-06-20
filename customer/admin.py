755ab4f15a66184b998cd441b428951c5c633942
Kosisookeke <k.okeke@alustudent.com>
2024-06-20 03:14:21 +0200

    done with some work

--- customer/admin.py
+++ customer/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
-from customer.models import Customer, Address, Card, Order
 
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Address)
-admin.site.register(Card)
-admin.site.register(Order)
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Restaurant, MenuItem, Order, OrderItem
-# Register your models here.
-
-admin.site.register(Restaurant)
-admin.site.register(MenuItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
