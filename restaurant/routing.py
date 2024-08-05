83a2bc05626a7b4a74385c1553a54cf70a53a211
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-08-05 05:08:53 +0200

    will continue tomorrow

--- restaurant/routing.py
+++ restaurant/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/restaurant/', consumers.RestaurantConsumer.as_asgi()),
+    re_path(r'ws/restaurant/(?P<id>\d+)/$', consumers.RestaurantConsumer.as_asgi()),
 ]
