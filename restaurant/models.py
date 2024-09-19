a86308a17940a3916e2de0853c5db5f5da72484e
EmmanuelBegati <e.yakubu@alustudent.com>
2024-09-19 15:17:01 +0200

    adjusted the categories

--- restaurant/models.py
+++ restaurant/models.py
@@ -123,7 +123,7 @@ class MenuItem(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    # image = models.ImageField(upload_to='categories/', null=True, blank=True)
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)
 
     def __str__(self):
         return self.name
