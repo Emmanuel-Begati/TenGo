3074929eefc235305a4e07aad1a3b35be7d31626
Emmanuel-Begati <begati16@gmail.com>
2025-03-07 12:42:34 +0200

    databaseurl fixed

--- TenGo/settings.py
+++ TenGo/settings.py
@@ -133,14 +133,7 @@ env = environ.Env(
 )
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT', default='5432'),
-    }
+   'default': env.db(),
 }
 
 
